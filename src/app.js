674bf6426645201ab06531f4e31198933e687958
Fogel Egor <fogel_92@bk.ru>
2022-07-03 15:29:26 +0700

    feat: add markup and validation

--- src/app.js
+++ src/app.js
@@ -1,1 +1,31 @@
-console.log("App is running");
+import { isValid } from './utils';
+import './styles.css';
+
+const form = document.getElementById('form');
+const input = form.querySelector('#question-input');
+const submitBtn = form.querySelector('#submit');
+
+form.addEventListener('submit', submitFormHandler);
+input.addEventListener('input', () => {
+  submitBtn.disabled = !isValid(input.value);
+});
+
+function submitFormHandler(e) {
+  e.preventDefault();
+
+  if (isValid(input.value)) {
+    const question = {
+      text: input.value.trim(),
+      date: new Date().toJSON(),
+    };
+
+    submitBtn.disabled = true;
+
+    //Async request to server to save question
+
+    console.log('Question', question);
+    input.value = '';
+    input.className = '';
+    submitBtn.disabled = false;
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -17,4 +17,12 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 };

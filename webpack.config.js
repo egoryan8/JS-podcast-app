117422e7ef662bc259666c32a76ba40ddee87805
Fogel Egor <fogel_92@bk.ru>
2022-07-03 14:38:56 +0700

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    port: 3000,
+  },
+};
